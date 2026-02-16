23e89c31fa53f4f9d89762e21b63cb8a0936afb0
Kanak <knk@gmail.com>
2026-02-16 22:25:52 +0530

    Initial commit: Setup Vite React app with React Router and base Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import  {assets} from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="logo" />
+      <ul>
+        <NavLink>
+            <li>Home</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>Doctors</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>About</li>
+            <hr />
+        </NavLink>
+        <NavLink>
+            <li>Contact</li>
+            <hr />
+        </NavLink>
+      </ul>
+      <div>
+        <button>
+            Create Account
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
