6c8329eb8985af321ca606f3e968a9d25e893081
Kanak <knk@gmail.com>
2026-02-19 23:15:59 +0530

    worked on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,28 +4,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="logo" />
-      <ul>
-        <NavLink>
-            <li>Home</li>
-            <hr />
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+      <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink to='/'>
+            <li className = 'py-1'>Home</li>
+            <hr className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
-        <NavLink>
-            <li>Doctors</li>
-            <hr />
+        <NavLink to='/doctors'>
+            <li className = 'py-1'>Doctors</li>
+            <hr className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden '/>
         </NavLink>
-        <NavLink>
-            <li>About</li>
-            <hr />
+        <NavLink to='/about'>
+            <li className = 'py-1'>About</li>
+            <hr className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto  hidden'/>
         </NavLink>
-        <NavLink>
-            <li>Contact</li>
-            <hr />
+        <NavLink to='/contact'>
+            <li className = 'py-1'>Contact</li>
+            <hr className = 'border-none outline-none h-0.5 bg-primary w-3/5 m-auto  hidden'/>
         </NavLink>
       </ul>
-      <div>
-        <button>
+      <div className='flex items-center gap-4'>
+        <button className = 'bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>
             Create Account
         </button>
       </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-})
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0056b3', 
+      }
+    },
+  },
+  plugins: [],
+}
