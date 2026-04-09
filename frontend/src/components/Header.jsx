65212ad4b503764cb337b21635360f82ae34edac
Kanak <rathorkanak03@gmail.com>
2026-04-09 20:49:01 +0530

    feat: add speciality menu in home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,18 +9,18 @@ const Header = () => {
       flex-col
       md:flex-row
       flex-wrap
-      bg-gradient-to-r from-green-800 to-blue-500
+      bg-linear-to-r from-green-800 to-blue-500
       rounded-lg
       px-6
       md:px-10
       lg:px-20"
     >
       {/* ---Left----*/}
-      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:-mb-[30px]">
+      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:-mb-30px">
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
           All Treatments <br /> UNDER ONE ROOF!
         </p>
-        <div className="flex flex-col md:flex-row items:center gap-3 text-white text-sm font-light">
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
           <img className="w-28" src={assets.group_profiles} alt="" />
           <p>
             Simply browse through our extensive list of trusted doctors,
