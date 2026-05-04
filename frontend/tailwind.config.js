b13133d397b1cb391704e93b2a30496567763421
Kanak <rathorkanak03@gmail.com>
2026-05-04 16:35:25 +0530

    create doctor section and add route to the doctor information page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#0056b3', 
-      }
+        primary: "#0056b3",
+      },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fill, minmax(200px, 1fr))",
+      },
     },
   },
   plugins: [],
-}
+};
