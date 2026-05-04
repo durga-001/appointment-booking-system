5baada4c61e8917cf5a66dad821abcd540bbec4a
Kanak <rathorkanak03@gmail.com>
2026-05-04 20:53:32 +0530

    fix doctors list issue

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import { doctors } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { useContext } from "react";
 
 const TopDoctors = () => {
   const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
+
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Our Top Doctors</h1>
       <p className="sm:w-1/3 text-center text-sm">
-        Browse through our expensive list of trusted doctors.
+        Browse through our extensive list of trusted doctors.
       </p>
-      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            onClick={() => navigate(`/appoitment/$(item._id)`)}
+            onClick={() => navigate(`/appointment/${item._id}`)}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-2.5 transition-all duration-500"
             key={index}
           >
-            <img className="bg-blue-50" src={item.image} alt="" />
+            <img
+              className="bg-blue-50 w-full object-cover"
+              src={item.image}
+              alt=""
+            />
             <div className="p-4">
-              <div className="flex items-center gap-2 text-sm text-center text-green-500">
+              <div className="flex items-center gap-2 text-sm text-green-500">
                 <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                 <p>Available</p>
               </div>
@@ -29,8 +37,15 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button className="bg-blue-500 text-white px-12 py-3 rounded-full mt-10">
-        more
+
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-500 text-white px-12 py-3 rounded-full mt-10"
+      >
+        More
       </button>
     </div>
   );
