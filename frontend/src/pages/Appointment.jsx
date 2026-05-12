d35c96d1a86808443189469e37c4b955bc2595ee
Kanak <rathorkanak03@gmail.com>
2026-05-12 17:05:58 +0530

    update doctors details

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { useEffect } from "react";
+import { assets } from "../assets/assets";
 
 const Appointment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { docId } = useParams();
+  const { doctors, currencySymbol } = useContext(AppContext);
 
-export default Appointment
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return (
+    docInfo && (
+      <div>
+        {/* Doctor Details */}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
+          </div>
+
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 mt-20 sm:mt-0">
+            {/* DocInfo name, degree, experience */}
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}
+              <img className="w-5" src={assets.verified_icon} alt="" />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+
+            {/* Doctor About */}
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-175 mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p>
+              Appointment Fee:{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default Appointment;
--- frontend/src/pages/MyAppointment.jsx
+++ frontend/src/pages/MyAppointment.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const MyAppointment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { docId } = useParams();
+  const { doctors } = useContext(AppContext);
 
-export default MyAppointment
+  const [docInfo, setDocInfo] = useState(null);
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInfo);
+    console.log(docInfo);
+  };
+
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
+
+  return <div></div>;
+};
+
+export default MyAppointment;
