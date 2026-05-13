2635245ed3bcf45f853771274c549b57c99395fd
Kanak <rathorkanak03@gmail.com>
2026-05-13 19:43:42 +0530

    top doctors with speciality

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const RelatedDoctors = ({ docId, speciality }) => {
   const { doctors } = useContext(AppContext);
   const [relDoc, setRelDocs] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (doctors.length > 0 && speciality) {
@@ -13,7 +15,44 @@ const RelatedDoctors = ({ docId, speciality }) => {
       setRelDocs(doctorsData);
     }
   }, [doctors, speciality, docId]);
-  return <div></div>;
+
+  return (
+    <div>
+      <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+        <h1 className="text-3xl font-medium">Our Top Doctors</h1>
+        <p className="sm:w-1/3 text-center text-sm">
+          Browse through our extensive list of trusted doctors.
+        </p>
+
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 px-3 sm:px-0">
+          {relDoc.slice(0, 5).map((item, index) => (
+            <div
+              onClick={() => {
+                navigate(`/appointment/${item._id}`);
+                scrollTo(0, 0);
+              }}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-2.5 transition-all duration-500"
+              key={index}
+            >
+              <img
+                className="bg-blue-50 w-full h-48 object-cover"
+                src={item.image}
+                alt={item.name}
+              />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-green-500">
+                  <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                  <p>Available</p>
+                </div>
+                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                <p className="text-gray-600 text-sm">{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RelatedDoctors;
--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -17,7 +17,10 @@ const TopDoctors = () => {
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 px-3 sm:px-0">
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            onClick={() => navigate(`/appointment/${item._id}`)}
+            onClick={() => {
+              navigate(`/appointment/${item._id}`);
+              scrollTo(0, 0);
+            }}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-2.5 transition-all duration-500"
             key={index}
           >
