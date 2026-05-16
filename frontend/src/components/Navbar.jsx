99e685ffc63d64bf9fdfb9b369d6705f4f78d762
Kanak <rathorkanak03@gmail.com>
2026-05-16 23:15:10 +0530

    profile section issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-700 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
-                  onClick={() => navigate("/profile")}
+                  onClick={() => navigate("/my-profile")}
                   className="hover:text-black cursor-pointer"
                 >
                   Profile
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { assets } from "../assets/assets";
 
 const MyProfile = () => {
+  const [userData, setUserData] = useState({
+    name: "Isha Sharma",
+    image: assets.profile_pic,
+    email: "ishant@gmail.com",
+    phone: "+1 123 456 7890",
+    address: {
+      line1: "Road cross-12, Diamon Way",
+      line2: "New Road, Norves",
+    },
+    gender: "Male",
+    dob: "2001-12-21",
+  });
+
+  const [isEdit, isSetEdit] = useState(false);
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <img src={userData.image} alt="" />
+      {isEdit ? (
+        <input
+          type="text"
+          value={userData.name}
+          onChange={(e) =>
+            setUserData((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
 
-export default MyProfile
+      <hr />
+      <div>
+        <p>Contact Information</p>
+        <div>
+          <p>Email id:</p>
+          <p>{userData.email}</p>
+          <p>Phone:</p>
+          {isEdit ? (
+            <input
+              type="text"
+              value={userData.phone}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, phone: e.target.value }))
+              }
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+
+          <p>Address:</p>
+          {isEdit ? (
+            <p>
+              <input type="text" />
+              <br />
+              <input type="text" />
+            </p>
+          ) : (
+            <p>
+              {userData.address.line1}
+              <br />
+              {userData.address.line2}
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
